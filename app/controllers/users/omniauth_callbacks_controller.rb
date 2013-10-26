6544dca5380faf6e978da2914079fe35d1173929
bluswtr <bluswtr@gmail.com>
2013-10-25 22:03:42 -0700

    app hanging caused by too many connections to localhost. facebook worker is working with sidekiq. sidekiq dashboard mounted and working

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,12 +1,21 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def facebook
+    auth = request.env["omniauth.auth"]
     # You need to implement the method below in your model (e.g. app/models/user.rb)
-    @user = User.find_for_facebook_oauth(request.env["omniauth.auth"], current_user)
+    @user = User.find_for_facebook_oauth(auth, current_user)
 
     if @user.persisted?
       sign_in_and_redirect @user, :event => :authentication #this will throw if @user is not activated
       set_flash_message(:notice, :success, :kind => "Facebook") if is_navigational_format?
       #User.create_friendlist(request.env["omniauth.auth"])
+      #@db_user = User.where(:provider => auth.provider, :uid => fb_uid).first
+
+      ##
+      # if the user is NEW or a Facebook Realtime Update was initiated
+      # the flag 'update_fb_friends' in the user object will be raised
+      if(@user.update_fb_friends && defined? @user.uid)
+        FacebookWorker.perform_async(auth.credentials.token,auth.uid.to_s,@user.id.to_s)
+      end
     else
       session["devise.facebook_data"] = request.env["omniauth.auth"]
       redirect_to new_user_registration_url
--- app/workers/facebook_worker.rb
+++ app/workers/facebook_worker.rb
@@ -3,27 +3,24 @@ include Sidekiq::Worker
 require 'open-uri'
 
 	def perform(token,uid,db_uid)
-		#fb_url = "https://graph.facebook.com/656118299?fields=id,name,friends.fields(picture)"
 		fb_url = "https://graph.facebook.com/"
-		#pic_endpnt = "/picture?redirect=false&access_token="
-		#friends_endpnt = "/friends?access_token="
 		endpoint = "?fields=friends.fields(id,name,username,picture)&access_token="
-		#response = URI.parse(fb_url+uid+friends_endpnt+token).read
 		response = URI.parse(fb_url+uid+endpoint+token).read
 		friendlist_hash = JSON.parse response
-
 		user = User.find(db_uid)
+		email = ""
 
 		friendlist_hash["friends"]["data"].each do |contact|
-			# id = contact["id"].to_s
-			# img_response = URI.parse(fb_url+id+pic_endpnt+token).read
-			# image_hash = JSON.parse img_response
-			# user.contacts << Contact.new(
-			# 				fb_uid: contact["id"],
-			# 				name:   contact["name"], 
-			# 				email: 	contact["username"] + "@facebook.com",
-			# 				image:  contact["picture"]["data"]["url"])
+			if contact["username"] 
+				email = contact["username"] + "@facebook.com"
+			end
+			user.contacts << Contact.new(
+					fb_uid: contact["id"],
+					name:   contact["name"], 
+					email: 	email,
+					image:  contact["picture"]["data"]["url"])
 		end
+
 		user.update_attribute(:update_fb_friends, false)
 	end
 end
