ecca19b9122ec9c4b250d76eb32388b05b9ec2e8
bluswtr <bluswtr@gmail.com>
2014-08-12 15:55:43 -0700

    added ability to download Facebook friend list or Strava rides even if already logged in as fb, bwm or strava user

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -5,39 +5,78 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   # ie: User needs to sign out and then sign back in to invoke these callbacks  
   def facebook
     auth = request.env["omniauth.auth"]
-    # You need to implement the method below in your model (e.g. app/models/user.rb)
-    @user = User.find_for_facebook_oauth(auth, current_user)
+    bikewithme_log("devise auth: #{auth}")
 
-    if @user.persisted?
-      sign_in_and_redirect @user, :event => :authentication #this will throw if @user is not activated
-      set_flash_message(:notice, :success, :kind => "Facebook") if is_navigational_format?
-      # indicate to users who know this new user by flagging the new user's contact records
-      Contact.update_new_user_contact(auth.uid.to_s,"",user_url(@user.id))      ##
-      # if the user is NEW or a Facebook Realtime Update was initiated
-      # 'update_fb_friends' (in the user object) will be set to true
-      # create new Contact for new User
-      if @user.update_fb_friends
-        FacebookWorker.perform_async(auth.credentials.token,auth.uid.to_s,@user.id.to_s)
-      end
+    # if logged in as a bwm or strava user, don't create fb login credentials, just download their friendlist
+    if user_signed_in? && (is_bwm_user(current_user) || is_strava_user(current_user))
+        bikewithme_log("authorized fb user and is currently logged in as bwm or strava user")
+        if current_user.update_fb_friends
+            Contact.update_new_user_contact(auth.uid.to_s,"",user_url(current_user.id))
+            FacebookWorker.perform_async(auth.credentials.token,auth.uid.to_s,current_user.id.to_s)
+            redirect_to to_app_invite_index_url
+        end
     else
-      session["devise.facebook_data"] = request.env["omniauth.auth"]
-      redirect_to new_user_registration_url
+        bikewithme_log("authorized fb user but is not currently logged in as bwm or strava user")
+        @user = User.find_for_facebook_oauth(auth,current_user)
+        if @user.persisted? #if is a facebook user
+            bikewithme_log("user persisted")
+            sign_in_and_redirect @user, :event => :authentication #this will throw if @user is not activated
+            set_flash_message(:notice, :success, :kind => "Facebook") if is_navigational_format?
+
+            # if the user is NEW or a Facebook Realtime Update was initiated
+            # 'update_fb_friends' (in the user object) will be set to true
+            # create new Contact for new User
+            if @user.update_fb_friends
+                bikewithme_log("updating user's contacts and downloading friendlist")
+                # indicate to users who know this new user by flagging the new user's contact records
+                Contact.update_new_user_contact(auth.uid.to_s,"",user_url(@user.id))
+                FacebookWorker.perform_async(auth.credentials.token,auth.uid.to_s,@user.id.to_s)
+            end
+        else
+            session["devise.facebook_data"] = request.env["omniauth.auth"]
+            redirect_to new_user_registration_url
+        end
     end
+  end
+
+  def is_bwm_user(user)
+    user.strava_uid.blank? && user.uid.blank?
+  end
+
+  def is_strava_user(user)
+    !user.strava_uid.blank?
+  end
+
+  def is_fb_user(user)
+    !user.uid.blank?
   end
 
   def strava
     auth = request.env["omniauth.auth"]
-    @user = User.find_for_strava_oauth(auth,current_user)
-    if @user.persisted?
-      sign_in_and_redirect @user, :event => :authentication #this will throw if @user is not activated
-      set_flash_message(:notice, :success, :kind => "Strava") if is_navigational_format?
-      Contact.update_new_user_contact("",auth.strava_uid,user_url(@user.id))
-      if @user.update_strava_objects
-          StravaWorker.perform_async(auth.credentials.token,@user.id.to_s)
-      end
+    bikewithme_log("devise auth: #{auth}")
+
+    if user_signed_in? && (is_bwm_user(current_user) || is_fb_user(current_user))# otherwise just download their ridelist
+        if current_user.update_strava_objects
+            Contact.update_new_user_contact("",auth.strava_uid,user_url(current_user.id))
+            StravaWorker.perform_async(auth.credentials.token,current_user.id.to_s)
+            redirect_to eventpost_index_url
+        end
+    # if logged in as a bwm or facebook user, don't create strava login credentials
     else
-      session["devise.strava_data"] = request.env["omniauth.auth"]
-      redirect_to new_user_registration_url
+        @user = User.find_for_strava_oauth(auth,current_user)
+        
+        if @user.persisted?
+          sign_in_and_redirect @user, :event => :authentication #this will throw if @user is not activated
+          set_flash_message(:notice, :success, :kind => "Strava") if is_navigational_format?
+          if @user.update_strava_objects
+                Contact.update_new_user_contact("",auth.strava_uid,user_url(@user.id))
+                StravaWorker.perform_async(auth.credentials.token,@user.id.to_s)
+          end
+        else 
+          session["devise.strava_data"] = request.env["omniauth.auth"]
+          redirect_to new_user_registration_url
+        end
+
     end
   end
 
--- app/workers/strava_worker.rb
+++ app/workers/strava_worker.rb
@@ -44,41 +44,20 @@ class StravaWorker
 			end
 		end
 
-		friends = "athlete/friends?access_token="
-		friendlist_hash = JSON.parse URI.parse(strava_url+friends+token).read
-		friendlist_hash.each do |friend|
-			Contact.create_strava_contact(friend,user)
-		end
+		# friends = "athlete/friends?access_token="
+		# friendlist_hash = JSON.parse URI.parse(strava_url+friends+token).read
+		# friendlist_hash.each do |friend|
+		# 	Contact.create_strava_contact(friend,user)
+		# end
 
 		user.update_attribute(:update_strava_objects, false)
+		user.save
 	end
-
-	# def create_streams(token,db_uid)
-	# 	activities = "athlete/activities?access_token="
-	# 	activities_hash = strava_api(activities,token)
-	# 	activities_hash['athlete'].each do |activity|
-	# 		Event.create_stream(activity)
-	# 	end
-	# end
-
-	# def create_friends(token,db_uid)
-	# 	friends = "athlete/friends?access_token="
-	# 	friendlist_hash = strava_api(friends,token)
-	# 	friendlist_hash.each do |contact|
-	# 		Contact.create_strava_contact(friend)
-	# 	end
-	# end
 	
 	##
 	# Update User Objects
 	def update_user_objects(token,db_uid)
 		update_streams(token,db_uid)
 		update_friends(token,db_uid)
-	end
-
-	def update_streams(token,db_uid)
-	end
-
-	def update_friends(token,db_uid)
 	end
 end
